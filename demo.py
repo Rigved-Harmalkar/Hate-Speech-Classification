14617f3b6e85d26c1dd2c4f97928aec0b0974cba
Rigved Harmalkar <rigved.harmalkar123@gmail.com>
2024-12-17 20:11:54 +0000

    added gcp files

--- demo.py
+++ demo.py
@@ -1,13 +1,8 @@
 from Hate.logger import logging
 from Hate.exception import CustomException
 import sys
+from Hate.configuration.gcloud_syncer import GCloudSync
 
 
-
-# logging.info("Welcome to our Project")
-
-try:
-    a = 7 / "0"
-    
-except Exception as e:
-    raise CustomException(e,sys) from e
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("hate_speech_rig","dataset.zip","dataset.zip")
