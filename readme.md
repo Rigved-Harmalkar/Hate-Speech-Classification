d0d16a81dfdff17b7f67a9f6550567792c2ee35c
Rigved Harmalkar <rigved.harmalkar123@gmail.com>
2024-12-17 18:08:20 +0000

    added requirements and utils

---
+++ readme.md
@@ -0,0 +1,16 @@
+
+
+
+
+```bash
+conda create -n hate python=3.8 -y
+
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name = "hate-speech-classification",
+    version = "0.0.1",
+    author = "Rigved",
+    author_email = "rigved.harmalkar123@gmail.com",
+    packages = find_packages(),
+    install_requires = [],
+    )
